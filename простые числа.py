a5ea2c2e327e509baf8863e0306850836ef3ffdd
Nikita46-ai <nikitaivanov0730@gmail.com>
2025-05-07 12:28:10 +0500

    Add files via upload

---
+++ простые числа.py
@@ -0,0 +1,42 @@
+
+number=2
+k=2
+
+
+while number!=200:
+    number += 1
+    while number%k != 0:
+        k+=1
+    else:
+        if k == number:
+            print(number)
+        k=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
